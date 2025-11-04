0ba0c443202357e914cbbb156acfb906b1f1c427
hkmarcoot <hkmarcoot@gmail.com>
2025-11-04 22:28:07 +0000

    Fix errors when deploying to Vercel.

--- pages/api/check-subscription.ts
+++ pages/api/check-subscription.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { getSubsciption, getUserInfo } from "./_common";
+import { getSubsciption, getUserInfo } from "./lib/common";
 
 import Cors from "cors";
 
